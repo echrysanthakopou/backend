0b6eca04a785e39281567268e36596b2744b66b0
evelin <echrysanthakopou@gmail.com>
2021-06-13 15:20:33 +0300

    User Entity DAO

---
+++ src/main/java/gr/pension/app/dao/UserEntityDAO.java
@@ -0,0 +1,22 @@
+package gr.pension.app.dao;
+
+import gr.pension.app.model.entities.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface UserEntityDAO extends CrudRepository<UserEntity, Integer> {
+    @Override
+    List<UserEntity> findAll();
+
+
+        List<UserEntity> findAllById(Integer id);
+
+
+    @Override
+    <S extends UserEntity> S save(S s);
+
+}
