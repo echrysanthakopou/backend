c77e9c7cc587cb4d9dafa94ec7312eb3fef944b1
evelin <echrysanthakopou@gmail.com>
2021-06-13 15:18:59 +0300

    Company DAO

---
+++ src/main/java/gr/pension/app/dao/CompanyDAO.java
@@ -0,0 +1,22 @@
+package gr.pension.app.dao;
+
+import gr.pension.app.model.entities.CompanyEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface CompanyDAO extends CrudRepository<CompanyEntity, Integer> {
+    @Override
+    List<CompanyEntity> findAll();
+
+
+        List<CompanyEntity> findAllById(Integer id);
+
+
+    @Override
+    <S extends CompanyEntity> S save(S s);
+
+}
