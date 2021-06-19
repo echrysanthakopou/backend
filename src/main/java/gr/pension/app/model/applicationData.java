a7516fba6c6be13e089c9d137bd8a8fc5d1c3798
evelin <echrysanthakopou@gmail.com>
2021-06-19 20:52:22 +0300

    Application related data

---
+++ src/main/java/gr/pension/app/model/applicationData.java
@@ -0,0 +1,51 @@
+package gr.pension.app.model;
+
+import java.sql.Date;
+
+public class applicationData {
+
+    public String name;
+    public String email;
+    public String type;
+    public Date date;
+
+    public applicationData(String name, String email, String type, Date date) {
+        this.name = name;
+        this.email = email;
+        this.type = type;
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
+
