e41c67e40b73ad784c332dc1adb14447c674540d
evelin <echrysanthakopou@gmail.com>
2021-06-19 20:49:13 +0300

    Create class with constant variable

---
+++ src/main/java/gr/pension/app/model/constants/Constants.java
@@ -0,0 +1,11 @@
+package gr.pension.app.model.constants;
+
+public class Constants {
+
+    //General Constant
+    public static String NAME="Pension backend App";
+
+
+    //Pensions limit
+    public static Long OLD_AGE_LIMIT=55L;
+}
